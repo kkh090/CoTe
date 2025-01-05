9ae2f3d8831f5b3a0f0d0ce50a5ade8fe35d2ca1
Ki Hoon Kwak <50243003+kkh090@users.noreply.github.com>
2025-01-05 19:45:49 +0900

    [Silver II] Title: 부분수열의 합, Time: 244 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ Python/백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,3 +1,20 @@
+# import sys
+# from itertools import combinations
+# input = sys.stdin.readline
+
+# N, S = map(int, input().split())
+
+# seq = list(map(int, input().split()))
+
+# answer = 0
+
+# for i in range(1, N+1):
+#     for x in combinations(seq, i):
+#         if sum(x) == S:
+#             answer += 1
+
+# print(answer)
+
 import sys
 from itertools import combinations
 input = sys.stdin.readline
@@ -8,9 +25,20 @@ seq = list(map(int, input().split()))
 
 answer = 0
 
-for i in range(1, N+1):
-    for x in combinations(seq, i):
-        if sum(x) == S:
-            answer += 1
+def subset_sum(idx, sub_sum):
+    global answer
+    
+    if idx >= N:
+        return
 
+    sub_sum += seq[idx]
+    
+    if sub_sum == S:
+        answer += 1
+    
+    subset_sum(idx+1, sub_sum)
+    subset_sum(idx+1, sub_sum - seq[idx])
+    
+subset_sum(0, 0)
 print(answer)
+    
